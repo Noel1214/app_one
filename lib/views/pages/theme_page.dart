3c3155ccee79bc0b2e19301e0dfb4376817a6d57
Noel Sebu <noelsebu555@gmail.com>
2025-09-01 11:30:54 +0530

    created themePage

---
+++ lib/views/pages/theme_page.dart
@@ -0,0 +1,39 @@
+import 'package:appone/data/notifiers.dart';
+import 'package:flutter/material.dart';
+
+class ThemePage extends StatelessWidget {
+  const ThemePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: selectedThemeNotifier,
+      builder: (context, value, child) {
+        return Container(
+          child: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                selectedThemeNotifier.value = !selectedThemeNotifier.value;
+              },
+              child: Container(
+                width: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    selectedThemeNotifier.value
+                        ? Icon(Icons.dark_mode)
+                        : Icon(Icons.light_mode),
+                    SizedBox(width: 10),
+                    selectedThemeNotifier.value
+                        ? Text("Dark Mode")
+                        : Text("Light Mode"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
