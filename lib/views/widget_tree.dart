bfdd8260c32eb494c0adddafe3b797828f3bc4c6
Noel Sebu <noelsebu555@gmail.com>
2025-09-01 11:31:20 +0530

    added theme page to pages list

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,10 +1,11 @@
 import 'package:appone/data/notifiers.dart';
 import 'package:appone/views/pages/home_page.dart';
 import 'package:appone/views/pages/profile_page.dart';
+import 'package:appone/views/pages/theme_page.dart';
 import 'package:flutter/material.dart';
 import 'package:appone/views/widgets/navbar_widgets.dart';
 
-List<Widget> pages = [HomePage(), ProfilePage()];
+List<Widget> pages = [HomePage(), ProfilePage(), ThemePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -13,9 +14,6 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Title"), centerTitle: true),
-      // body: currentIndex == 0
-      //     ? Center(child: Text("Home", style: TextStyle(fontSize: 30)))
-      //     : Center(child: Text("Person", style: TextStyle(fontSize: 30))),
       body: ValueListenableBuilder(
         valueListenable: selectedPageNotifier,
         builder: (context, value, child) {
