ffeb76ae1453fda3888d2ae7c650855ccc58ace2
Noel Sebu <noelsebu555@gmail.com>
2025-09-01 10:56:50 +0530

    change page according to value notifier value

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,13 +1,10 @@
+import 'package:appone/data/notifiers.dart';
 import 'package:appone/views/pages/home_page.dart';
 import 'package:appone/views/pages/profile_page.dart';
 import 'package:flutter/material.dart';
-import 'package:appone/widgets/navbar_widgets.dart';
+import 'package:appone/views/widgets/navbar_widgets.dart';
 
-List<Widget> pages = [
-  HomePage(),
-  ProfilePage(),
-];
-
+List<Widget> pages = [HomePage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -15,11 +12,17 @@ class WidgetTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(title: Text("Title"), centerTitle: true),
-          // body: currentIndex == 0
-          //     ? Center(child: Text("Home", style: TextStyle(fontSize: 30)))
-          //     : Center(child: Text("Person", style: TextStyle(fontSize: 30))),
-          bottomNavigationBar: NavbarWidget(),
-        );
+      appBar: AppBar(title: Text("Title"), centerTitle: true),
+      // body: currentIndex == 0
+      //     ? Center(child: Text("Home", style: TextStyle(fontSize: 30)))
+      //     : Center(child: Text("Person", style: TextStyle(fontSize: 30))),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, value, child) {
+          return pages.elementAt(selectedPageNotifier.value);
+        },
+      ),
+      bottomNavigationBar: NavbarWidget(),
+    );
   }
 }
